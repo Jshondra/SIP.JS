0bb39786d9f91c52311001521f2a0970a27f3951
root <root@DESKTOP-IEOUJ2T.localdomain>
2021-09-21 15:07:48 +0300

    first_try_1

---
+++ sip_ctx_1/phone/scripts/config.js
@@ -0,0 +1,12 @@
+var user = {
+    //  User Name
+    "User" : "123",
+    //  Password
+    "Pass" : "pwd",
+    //  Auth Realm
+    "Realm"   : "10.34.201.13",
+    // Display Name
+    "Display" : "Gob Bleuth",
+    // WebSocket URL
+    "WSServer"  : "ws://10.34.201.13:5062"
+};
